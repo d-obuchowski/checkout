46d7d0a8b3e3925745c865bdc33e64ed7ed06214
d-obuchowski <darek-ziomek16@o2.pl>
2020-12-21 13:30:26 +0100

    fixes

--- checkout.rb
+++ checkout.rb
@@ -1,4 +1,9 @@
+require "bigdecimal"
+
 class Checkout
+
+  SCARF_REGULAR_PRICE = BigDecimal("9.25")
+  SCARF_PROMOTION_PRICE = BigDecimal("8.50")
 
   def initialize(promotional_rules)
     @total_price = BigDecimal(0)
@@ -24,33 +29,25 @@ class Checkout
   end
 
   def calculate_total_price_with_promotions
-    @total_price_with_promotions = @total_price
+    total_price_with_promotions = @total_price
 
     promotional_rules.each do |prom|
-      @total_price_with_promotions-= self.send("calculate_reduced_price_#{prom}_prom")
+      total_price_with_promotions-= self.send("calculate_reduced_price_#{prom}_prom", total_price_with_promotions)
     end
 
-    @total_price_with_promotions
+    total_price_with_promotions
   end
 
-  def calculate_reduced_price_spending_over_60_prom
-    return 0 unless @total_price_with_promotions > 60
+  def calculate_reduced_price_spending_over_60_prom(total_price_with_promotions)
+    return 0 unless total_price_with_promotions > 60
 
-    @total_price_with_promotions * BigDecimal("0.10")
+    total_price_with_promotions * BigDecimal("0.10")
   end
 
-  def calculate_reduced_price_purchasing_red_scarf_prom
+  def calculate_reduced_price_purchasing_red_scarf_prom(total_price_with_promotions)
     return 0 unless scarf_items_size >= 2
 
-    scarf_items_size * BigDecimal(scarf_regular_price - scarf_promotion_price)
-  end
-
-  def scarf_regular_price
-    BigDecimal("9.25")
-  end
-
-  def scarf_promotion_price
-    BigDecimal("8.50")
+    scarf_items_size * BigDecimal(SCARF_REGULAR_PRICE - SCARF_PROMOTION_PRICE)
   end
 
   def scarf_items_size
